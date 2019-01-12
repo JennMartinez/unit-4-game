050a48e46ed30026351cee6771f89ffb77816660
Jennifer M <jennifermartinez@Jennifers-MacBook-Pro.local>
2019-01-12 12:09:12 -0800

    Crystal Game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,9 +12,7 @@ var low = 1;
 var high = 12;
 
 // Document Ready Event //
-$(document).ready(function(){
-
-// A random number needs to be generated and displayed at the start of the game, 19 - 120 //
+$(document).ready(function() {
 
 console.log(crystalValue);
 resetGame();
@@ -31,26 +29,30 @@ $("#purple").on("click", function() {
     console.log(totalScore);
 // Generates the sum between the crystalValue and totalScore, after each each click //
     $("#totalScore").html(totalScore);
+// Adds the wins/losses to the counter //
+    counts ();
 });
 
 $("#blue").on("click", function() {
     totalScore = totalScore + crystalValue[1];
     console.log(totalScore);
     $("#totalScore").html(totalScore);
+    counts ();
 });
 
 $("#light").on("click", function() {
     totalScore = totalScore + crystalValue[2];
     console.log(totalScore);
     $("#totalScore").html(totalScore);
+    counts ();
 });
 
 $("#white").on("click", function() {
     totalScore = totalScore + crystalValue[3];
     console.log(totalScore);
     $("#totalScore").html(totalScore);
+    counts ();
 });
-// The total score will generate a sum by adding the two values (crystalValue and totalScore) after each click //
 
 // Restart the game after a WIN or LOSS //
 // The player's score counter will reset to zero //
@@ -58,8 +60,9 @@ $("#white").on("click", function() {
 // The four crystals will generate new hidden numbers //
 
 function resetGame() {
-    // Generates random number for the main number //
-    random = Math.floor(Math.random()*(max-min+1)+min);
+// A random number is generated and displayed at the start of the game, 19 - 120 //
+
+random = Math.floor(Math.random()*(max-min+1)+min);
     $(".randomNumber").html(random);
     console.log(random);
 
@@ -67,23 +70,26 @@ function resetGame() {
         crystalValue.push(Math.floor(Math.random()*(high-low+1)+low));
         console.log(crystalValue);  
     }
-
+} 
 // The player WINS if their score EQUALS the random number generated at the beginning //
 // The game should store the player's wins and losses //
+
+function counts () {
     if (totalScore === random) {
         wins++;
         $("#wins").html("Wins: " + wins);
-        totalScore = 0;
-        resetGame();
         console.log(wins);
+        totalScore = 0;
+        resetGame()
     }
 
 // The player LOSSES if their score IS NOT EQUAL to the random number generated at the beginning //
     else if (totalScore > random) {
         losses++;
         $("#losses").html("Losses: " + losses);
+        console.log(losses);
         totalScore = 0;
-        resetGame();
-    }
+        resetGame()
+   }
 }
 });
